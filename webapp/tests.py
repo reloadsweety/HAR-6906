f280a3b738876b127e506d0adc72cdfbc128bc54
KITTIPOOM <reg_devil@hotmail.com>
2017-12-14 01:47:35 +0700

    simple test

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexViewTest(TestCase):
+    def test_indexpage_is_avaliable(self):
+        respone = self.client.get(reverse("webapp:index"))
+        self.assertEqual(respone.status_code, 200)
