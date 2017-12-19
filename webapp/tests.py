3b472cc2fd551f2657103a69e38ad66ddd53273f
KITTIPOOM <reg_devil@hotmail.com>
2017-12-20 00:16:32 +0700

    add test case for webapp

--- webapp/tests.py
+++ webapp/tests.py
@@ -6,15 +6,98 @@ from .models import Todo
 
 class TodoModelTest(TestCase):
     def test_save_model(self):
+        '''
+            test create and save model to db
+        '''
         todo_test1 = Todo(todo_name="test1", create_date=timezone.now())
-        todo_test2 = Todo(todo_name="test1", create_date=timezone.now())
+        todo_test2 = Todo(todo_name="test2", create_date=timezone.now())
         todo_test1.save()
         todo_test2.save()
+        todo_lists = Todo.objects.all().order_by("-create_date")
+        self.assertQuerysetEqual(todo_lists, ['<Todo: test1>', '<Todo: test2>'])
 
-        self.assertEqual(len(Todo.objects.all()), 2)
+    def test_remove_model(self):
+        '''
+            test create and delete model with todo_name
+        '''
+        todo_test1 = Todo(todo_name="test1", create_date=timezone.now())
+        todo_test1.save()
+        todo_lists = Todo.objects.all().order_by("-create_date")
+        obj = Todo.objects.get(todo_name="test1")
+        obj.delete()
+        self.assertQuerysetEqual(todo_lists, [])
+
+
+def create_simple_list():
+    todo_test1 = Todo(id=50, todo_name="test1", create_date=timezone.now())
+    todo_test2 = Todo(id=51, todo_name="test2", create_date=timezone.now())
+    todo_test1.save()
+    todo_test2.save()
 
 
 class IndexViewTest(TestCase):
     def test_indexpage_is_avaliable(self):
-        respone = self.client.get(reverse("webapp:index"))
-        self.assertEqual(respone.status_code, 200)
+        response = self.client.get(reverse("webapp:index"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_indexpage_is_avaliable_and_show_tdodolists(self):
+        create_simple_list()
+        response = self.client.get(reverse("webapp:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context['todo_lists'],
+            ['<Todo: test1>', '<Todo: test2>']
+        )
+
+    def test_indexpage_when_add_todo_without_error_message_then_add_todo_and_redirect_to_indexpage(self):
+        '''
+        when post with url webapp:addtodo with todo_name if not have error_message then redirect to indexpage
+        with status code 302 and then check todo_lists again
+        '''
+        create_simple_list()
+        response = self.client.post(reverse("webapp:addtodo"), {'todo_name': 'test3'})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("webapp:index"))
+
+        response = self.client.get(reverse("webapp:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context['todo_lists'],
+            ['<Todo: test1>', '<Todo: test2>', '<Todo: test3>']
+        )
+
+    def test_indexpage_when_add_todo_with_blank_value_then_return_error_message(self):
+        '''
+        when post with url webapp:addtodo  with blank todo_name error_message
+        then return error_message is 'can not add todo with blank value' and then render indexpage again
+        '''
+        create_simple_list()
+        response = self.client.post(reverse("webapp:addtodo"), {'todo_name': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['error_message'], 'can not add todo with blank value')
+
+    def test_indexpage_when_add_todo_with_duplicate_value_then_return_error_message(self):
+        '''
+        when post with url webapp:addtodo  with blank todo_name error_message
+        then return error_message is 'todo is already exist' and then render indexpage again
+        '''
+        create_simple_list()
+        response = self.client.post(reverse("webapp:addtodo"), {'todo_name': 'test1'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['error_message'], 'todo is already exist')
+
+    def test_indexpage_when_remove_todo_then_remove_todo_and_redirect_to_indexpage(self):
+        '''
+        50 ,51 is todo.id for Test1 and Test2 form simple list
+        when post with url webapp:removetodo then redirect to indexpage
+        with status code 302 and then check todo_lists again
+        '''
+        create_simple_list()
+        response = self.client.post(reverse("webapp:removetodo"), {'todos': [50, 51]})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("webapp:index"))
+
+        response = self.client.get(reverse("webapp:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context['todo_lists'], [])
--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,7 @@
 from django.views import generic
 from .models import Todo
 from django.utils import timezone
-from django.shortcuts import redirect, reverse
+from django.shortcuts import redirect, reverse, render
 
 
 '''
@@ -11,7 +11,7 @@ def index(request):
 
     def index(request):
     context = {'todo_lists': Todo.objects.all()}
-    return render(request, 'pages/index.html', context)
+    return render(request, 'pages/index.html', context) reverse("webapp:index")
 '''
 
 
@@ -25,17 +25,24 @@ class IndexView(generic.ListView):
 
 def add_todo(request):
     name = request.POST.get('todo_name')
-    print("NAME : %s" % name)
-    if(name):
-        todo = Todo(todo_name=name, create_date=timezone.now())
-        todo.save()
+    context = {'error_message': '', 'todo_lists': Todo.objects.all().order_by("-create_date")}
+    todo = Todo(todo_name=name, create_date=timezone.now())
+    if name:
+        if len(Todo.objects.filter(todo_name=name)) == 0:
+            todo.save()
+        else:
+            context['error_message'] = 'todo is already exist'
+    else:
+        context['error_message'] = 'can not add todo with blank value'
+
+    if context['error_message']:
+        return render(request, 'pages/index.html', context)
+
     return redirect(reverse("webapp:index"))
 
 
 def remove_todo(request):
     todos = request.POST.getlist('todos')
-    print("todos : %s" % todos)
-
     for id in todos:
         obj = Todo.objects.get(pk=id)
         obj.delete()
